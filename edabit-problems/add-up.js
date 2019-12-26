24d2809e5538bfc37f93993e812e78d0b46996b9
Timothy Morrise <timothy.morrise@gmail.com>
2019-12-26 15:06:56 -0700

    add up

---
+++ edabit-problems/add-up.js
@@ -0,0 +1,3 @@
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+const addUp = (n) => (n*(n-1))/2
