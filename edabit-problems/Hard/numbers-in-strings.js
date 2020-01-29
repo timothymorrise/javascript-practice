4139d3646fd221e7380cffdd0ce278f03ee1dcf8
Timothy Morrise <timothy.morrise@gmail.com>
2020-01-28 19:35:18 -0700

    numbers in strings

---
+++ edabit-problems/Hard/numbers-in-strings.js
@@ -0,0 +1,3 @@
+// Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. If there are no strings containing numbers, return an empty array.
+
+const numInStr = a => a.filter(x => /\d/.test(x) )
