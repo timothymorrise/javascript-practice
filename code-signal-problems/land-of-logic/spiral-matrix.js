da1de52ab418256475582bca0fca96f5ee6d7020
Timothy Morrise <timothy.morrise@gmail.com>
2020-02-04 20:53:48 -0700

    spiral matrix update

--- code-signal-problems/land-of-logic/spiral-matrix.js
+++ code-signal-problems/land-of-logic/spiral-matrix.js
@@ -4,27 +4,27 @@ const spiralNumbers = n => {
     let a = new Array(n).fill().map(() => new Array().fill(''))
     let direction = "right",
         lengthOffSet = 0,
-        horizontalIndex = 0,
-        verticalIndex = 0
+        x = 0,
+        y = 0
     for (let i=1;i<((n*n)+1);i++) {
         if (direction==="right") {
-            a[verticalIndex][horizontalIndex]=i
-            horizontalIndex++
-            if(horizontalIndex===n-1-lengthOffSet) direction="down"
+            a[y][x]=i
+            x++
+            if(x===n-1-lengthOffSet) direction="down"
         } else if (direction==="down") {
-            a[verticalIndex][horizontalIndex]=i
-            verticalIndex++
-            if(verticalIndex===n-1-lengthOffSet) direction="left" 
+            a[y][x]=i
+            y++
+            if(y===n-1-lengthOffSet) direction="left" 
         } else if (direction==="left") {
-            a[verticalIndex][horizontalIndex]=i
-            horizontalIndex--
-            if(horizontalIndex===0+lengthOffSet) direction="up"   
+            a[y][x]=i
+            x--
+            if(x===0+lengthOffSet) direction="up"   
         } else if (direction==="up") {
-            a[verticalIndex][horizontalIndex]=i
-            verticalIndex--
-            if(verticalIndex===0+lengthOffSet) {
-                verticalIndex++
-                horizontalIndex++
+            a[y][x]=i
+            y--
+            if(y===0+lengthOffSet) {
+                y++
+                x++
                 lengthOffSet++
                 direction="right"
             }
