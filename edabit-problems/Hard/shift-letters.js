f595fbbed14b45f7bf4146db6c32abdf0d41c67f
Timothy Morrise <timothy.morrise@gmail.com>
2020-02-21 14:27:02 -0700

    shift letters, finished

--- edabit-problems/Hard/shift-letters.js
+++ edabit-problems/Hard/shift-letters.js
@@ -1,11 +1,16 @@
 // Create a function that takes a string and shifts the letters left by an amount n but not the whitespace.
 
-function shiftLetters(s, n) {
-	let whitespaces = []
-	let output = ""
+const shiftLetters = (s, n) => {
+	let spaces = []
 	a = s.split('').filter((x,i) => {
-		if (x===" ") whitespaces.push(i)
+		if (x===" ") spaces.push(i)
 		return x!==" "
 	})
-	console.log(a, whitespaces)
+	for (let i=0;i<n;i++) a.unshift(a.pop())
+	for (i=0;i<spaces.length;i++) a = [...a.slice(0,spaces[i]), " ", ...a.slice(spaces[i],a.length)]
+	return a.join("")
 }
+
+// NOTES
+// Elegance - didnt need to filter the array, couldn't used .replace
+// method to get the spaces out and then spread the string.
