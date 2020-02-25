021f40b75eff693dbad1e5a1e1a1c3fe5c53618e
Timothy Morrise <timothy.morrise@gmail.com>
2020-02-25 08:59:33 -0700

    combinations finished

--- edabit-problems/Hard/combinations.js
+++ edabit-problems/Hard/combinations.js
@@ -1,5 +1,5 @@
 // Create a function that takes a variable number of groups of items, and returns the number of ways the items can be arranged, with one item from each group. Order does not matter.
 
 function combinations(items) {
-	return [].slice.call(arguments).reduce((acc, x) => acc*x)
+	return [].slice.call(arguments).reduce((acc, x) => (x===0)?acc:acc*x)
 }
