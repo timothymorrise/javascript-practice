34f70856174666a365b55094d6e8baaec2132434
Timothy Morrise <timothy.morrise@gmail.com>
2020-03-28 23:54:54 -0600

    second right most bit

---
+++ code-signal/core/3-corner-of-0s-and-1s/second-right-most-bit.js
@@ -0,0 +1,6 @@
+// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+// Presented with the integer n, find the 0-based position of the second rightmost zero bit in its binary representation (it is guaranteed that such a bit exists), counting from right to left.
+
+const secondRightmostZeroBit = n => {
+    return (~((~n&(n+1))|n))&-(~((~n&(n+1))|n))
+  }
