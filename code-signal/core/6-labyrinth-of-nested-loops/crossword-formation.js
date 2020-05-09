2573941db14baca8ec789df108df25c4adbc76fd
Timothy Morrise <timothy.morrise@gmail.com>
2020-05-08 23:04:31 -0600

    crossword formation WIP II

--- code-signal/core/6-labyrinth-of-nested-loops/crossword-formation.js
+++ code-signal/core/6-labyrinth-of-nested-loops/crossword-formation.js
@@ -1,26 +1,36 @@
 const sans = (a,n) => a.filter((x,i)=> i!==n)
 
-const getLetterIntersections = (s1, s2) => {
+const getLetterIntersections = (skip, s1, s2) => {
     let a = []
     for (let i=0;i<s1.length;i++) {
         for (let j=0;j<s2.length;j++) {
-            if (s1[i]===s2[j]) a.push([s1[i], i, j])
+            if (s1[i]===s2[j] && i!==skip) a.push([s1[i], i, j])
         }
     }
     return a
 }
 
-const crosswordFormation = w => { //words=>
-    // compare each word to each other word
-    for (let i=0;i<4;i++) {
-        let a = sans(w,i)
-        for (let j=0;j<3;j++) {
-            console.log(getLetterIntersections(w[i],a[j]))
+    // compare the chosen word to it's compatriats
+    for (let i=0;i<4;i++) { 
+        let first = w[i] // define first word     
+        let a = sans(w,i) // filter array
+        for (let j=0;j<3;j++) {          
+            let second = a[j] // define second word          
+            let firstInstersections = getLetterIntersections(null,first,second)  // find all intersections between first two words
+            // compare the second word word to the third word in the array
+            let b = sans(a, j) // filter out second word
+            let third = b[0]
+            for ( let k=0;k<firstInstersections.length;k++) {          // loop through the intersection array
+                let secondIntersections = getLetterIntersections(firstInstersections[k][2],second,third) // get intersections of second and third
+                
+            }
+
+                // compare now against the fourth word matching the distances between indexes to make sure this work
+
+            // compare again but swap the third and four words from last time
         }
-        // compare a word to another word
         
-        // if there is a letter match, begin comparing a different word to that word for a match letter
-        // finally check the last word, 
+       
     }
 }
 
